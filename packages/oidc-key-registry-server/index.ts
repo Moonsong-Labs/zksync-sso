be16d400c0cc5f21af5dc53f73514c64dc468696
matias-gonz <maigonzalez@fi.uba.ar>
2025-01-31 12:21:56 -0300

    Add fetchKeys

--- packages/oidc-key-registry-server/index.ts
+++ packages/oidc-key-registry-server/index.ts
@@ -1,6 +1,45 @@
-const task = () => {
-  console.log('Hello World');
-  setTimeout(task, 1000);
+const fetchKeys = async (): Promise<Array<Key>> => {
+  const response = await fetch("https://www.googleapis.com/oauth2/v3/certs");
+  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+  const data: GoogleResponse = await response.json();
+  return data.keys.map(key => ({
+    kid: toBytes32(key.kid),
+    n: toBytes(key.n),
+    e: toBytes(key.e)
+  }));
 }
 
-task();
+interface Key {
+  kid: string;  // Key ID (as bytes32)
+  n: string;    // RSA modulus (as bytes)
+  e: string;    // RSA exponent (as bytes)
+}
+
+const toBytes32 = (str: string): string => {
+  return `0x${str.padStart(64, '0')}`;
+}
+
+const toBytes = (str: string): string => {
+  return `0x${Buffer.from(str, 'base64url').toString('hex')}`;
+}
+
+interface GoogleResponse {
+  keys: Array<KeyResponse>
+}
+
+interface KeyResponse {
+  "kid": string;
+  "n": string;
+  "e": string;
+}
+
+const main = async () => {
+  const keys = await fetchKeys();
+
+  console.log(keys);
+
+  setTimeout(main, 1000);
+}
+
+main();
