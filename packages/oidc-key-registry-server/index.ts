a38037576bf4e4bdb72fdd0e0866e710dfb15884
matias-gonz <maigonzalez@fi.uba.ar>
2025-02-03 08:15:28 -0300

    Refactor keyFetcher

--- packages/oidc-key-registry-server/index.ts
+++ packages/oidc-key-registry-server/index.ts
@@ -1,45 +1,16 @@
-const fetchKeys = async (): Promise<Array<Key>> => {
-  const response = await fetch("https://www.googleapis.com/oauth2/v3/certs");
-  if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-  const data: GoogleResponse = await response.json();
-  return data.keys.map(key => ({
-    kid: toBytes32(key.kid),
-    n: toBytes(key.n),
-    e: toBytes(key.e)
-  }));
-}
-
-interface Key {
-  kid: string;  // Key ID (as bytes32)
-  n: string;    // RSA modulus (as bytes)
-  e: string;    // RSA exponent (as bytes)
-}
-
-const toBytes32 = (str: string): string => {
-  return `0x${str.padStart(64, '0')}`;
-}
-
-const toBytes = (str: string): string => {
-  return `0x${Buffer.from(str, 'base64url').toString('hex')}`;
-}
-
-interface GoogleResponse {
-  keys: Array<KeyResponse>
-}
-
-interface KeyResponse {
-  "kid": string;
-  "n": string;
-  "e": string;
-}
+import { GoogleFetcher } from "./fetchers/google";
 
 const main = async () => {
-  const keys = await fetchKeys();
+  const fetcher = new GoogleFetcher();
 
-  console.log(keys);
+  try {
+    const keys = await fetcher.fetchKeys();
+    console.log("Fetched keys:", keys);
+  } catch (error) {
+    console.error("Error fetching keys:", error);
+  }
 
-  setTimeout(main, 1000);
-}
+  setTimeout(main, 60 * 1000);
+};
 
 main();
